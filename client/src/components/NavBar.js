d30a22f9c2b825558df92d7c8b71de90ddbb1931
Sergei2010 <sergejmatvejcuk323@gmail.com>
2021-11-25 11:31:26 +0200

    go from NavBar to Auth

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,36 +5,39 @@ import Nav from 'react-bootstrap/Nav'
 import { NavLink } from 'react-router-dom'
 import { SHOP_ROUTE } from '../utils/consts'
 
-import { Button } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
+import { observer } from 'mobx-react-lite'
 
-const NavBar = () => {
+const NavBar = observer(() => {
 	const { user } = useContext(Context)
 	return (
 		<Navbar bg='dark' variant='dark' className='d-flex justify-content-between'>
-			<NavLink style={{ color: 'white' }} to={SHOP_ROUTE} className='p-2'>
-				КупиДевайс
-			</NavLink>
-			{user.isAuth ? (
-				<Nav style={{ color: 'white' }} className='p-2'>
-					<Button variant={'outline-light'} className='me-2'>
-						Админ панель
-					</Button>
-					<Button variant={'outline-light'} className='me-2'>
-						Войти
-					</Button>
-				</Nav>
-			) : (
-				<Nav style={{ color: 'white' }} className='p-2'>
-					<Button variant={'outline-light'} className='me-2'>
-						Админ панель
-					</Button>
-					<Button variant={'outline-light'} className='me-2'>
-						Авторизация
-					</Button>
-				</Nav>
-			)}
+			<Container>
+				<NavLink style={{ color: 'white' }} to={SHOP_ROUTE} className='p-2'>
+					КупиДевайс
+				</NavLink>
+				{user.isAuth ? (
+					<Nav style={{ color: 'white' }} className='p-2'>
+						<Button variant={'outline-light'} className='me-2'>
+							Админ панель
+						</Button>
+						<Button variant={'outline-light'} className='me-2'>
+							Войти
+						</Button>
+					</Nav>
+				) : (
+					<Nav style={{ color: 'white' }} className='p-2'>
+						<Button variant={'outline-light'} className='me-2'>
+							Админ панель
+						</Button>
+						<Button variant={'outline-light'} className='me-2' onClick={() => user.setIsAuth(true)}>
+							Авторизация
+						</Button>
+					</Nav>
+				)}
+			</Container>
 		</Navbar>
 	)
-}
+})
 
 export default NavBar

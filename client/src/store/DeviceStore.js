3eb9de625ddcc4b49a6edecca99e10298762ee26
Sergei2010 <sergejmatvejcuk323@gmail.com>
2021-12-08 13:33:16 +0200

    go to Search

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -7,7 +7,10 @@ export default class DeviceStore {
 		this._devices = []
 		this._selectedType = {}
 		this._selectedBrand = {}
-		makeAutoObservable(this)
+		this._page = 1 // поле отвечает за текущую страницу
+		this._totalCount = 0 // общее количество товаров
+		this._limit = 4 // количество товаров на 1 странице
+		makeAutoObservable(this) // уловить существующие свойства объекта
 	}
 	setTypes(types) {
 		this._types = types
@@ -19,10 +22,18 @@ export default class DeviceStore {
 		this._devices = devices
 	}
 	setSelectedType(type) {
+		this.setPage(1)
 		this._selectedType = type
 	}
 	setSelectedBrand(brand) {
+		this.setPage(1)
 		this._selectedBrand = brand
+	}
+	setPage(page) {
+		this._page = page
+	}
+	setTotalCount(count) {
+		this._totalCount = count
 	}
 	get types() {
 		return this._types
@@ -38,5 +49,14 @@ export default class DeviceStore {
 	}
 	get selectedBrand() {
 		return this._selectedBrand
+	}
+	get totalCount() {
+		return this._totalCount
+	}
+	get page() {
+		return this._page
+	}
+	get limit() {
+		return this._limit
 	}
 }
